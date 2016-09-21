73e21594c17ea0e0b7f72abe47dc59636832d921
Gregory Bunting <gbuntin@sandia.gov>
2016-09-21 12:55:47 -0600

    weighted random

--- lunch.py
+++ lunch.py
@@ -5,116 +5,119 @@ print('\033c')
 print('The lunch location selected by this script is not negotiable:')
 print('')
 
-lunchList = [
-             'County Line',
-             'Sadie\'s',
-             'Cervante\'s',
-             'Leilani\'s Cafe',
-             'K & I Diner (the full travis)',
-             'Polittos con papas',
-             'Thunderbird',
-             'Red Rock Diner',
-             'Giovanni\'s',
-             'Dion\'s',
-             'Jimmy John\'s',
-             'Costco',
-             'Griff\'s',
-             'Tia Betty Blues',
-             'Tia Betty Waffles',
-             'Saggio\'s',
-             'Bahn Mi Coda',
-             'Cafe Trang',
-             'Freddy\'s',
-             'Thai Tip',
-             'Vegan Thai',
-             'Golden Pride',
-             'Blake\'s', 
-             'Bob\'s Burger',
-             'Sonic',
-             'The Tractor',
-             'Lizard Tail',
-             'La Salita',
-             'Chic fil a',
-             'Lynns House (call ahead)',
-             'Powdrell\'s',
-             'The Cube',
-             'Frontier',
-             'Two Fools',
-             'Subway',
-             'McDonald\'s (Kirtland)',
-             'Burger King', 
-             'Wendy\'s',
-             'Carl\'s Jr',
-             'Caesar\'s',
-             'Hurricanes',
-             'Taco Cabana',
-             'Chili\'s',
-             'Appleby\'s',
-             'TGI Friday\'s',
-             'B2B',
-             'Cheese and Coffee',
-             'Flying Star Cafe',
-             'Weck\'s',
-             'The Owl Cafe',
-             'Rudys',
-             'Cheesecake Factory',
-             'Chipotle',
-             'Taco Bell',
-             'Del Taco',
-             'Emissions testing place',
-             'Basil Leaf',
-             'Chen\'s',
-             'Fantang',
-             'Szechwan',
-             'Slice Parlor',
-             'Waffle House',
-             'Relish',
-             'Gino\'s New York Style',
-             'Stufy\'s',
-             'Cafe Istanbul (call to make sure their kitchen is open)',
-             'Taj Palace'
-             'Olive Garden',
-             'High Finance',
-             'Mall food court ()',
-             'Uptown Sports Pub',
-             'Panda Express',
-             'Boston Market',
-             'Los Cuates',
-             'El patio',
-             'Gardunos',
-             'O Ramen',
-             'Anapurna',
-             'Sweet Tomatoes',
-             'Super Salad',
-             'Jason\'s Deli',
-             'McCalister\'s',
-             'Golden Corral',
-             'UNM (the on in the dormitories)',
-             'Scallo',
-             'Tucano\'s',
-             'Copper Lounge',
-             'Kelly\'s',
-             'Pizza Hut',
-             'Domino\'s',
-             'Godfather Pizza',
-             'CiCi\'s Pizza',
-             'Mongolian BBQ',
-             'Lynn\'s Buffet',
-             'IHOP',
-             'Denny\'s',
-             'Vick\'s Vittles',
-             'Twister\'s',
-             'Hooter\'s,',
-             'Texas Roadhouse',
-             'Texas Land and Cattle',
-             'Santiago\'s',
-             'Sandia Chille Grille',
-             'Racetrack Casino',
-             'Sandia Casino'
-             'Whataburger',
-             'Weinersnitzel',
-             'Jack in the Box'
-            ]
+lunchList =  ['County Line'] * 1 +\
+             ['Sadie\'s'] * 5 +\
+             ['Cervante\'s'] * 5 +\
+             ['Leilani\'s Cafe'] * 2 +\
+             ['K & I Diner (the full travis)'] * 10 +\
+             ['Polittos con papas'] * 2 +\
+             ['Thunderbird'] * 3 +\
+             ['Red Rock Diner'] * 5 +\
+             ['Giovanni\'s'] * 10 +\
+             ['Dion\'s'] * 5 +\
+             ['Jimmy John\'s'] +\
+             ['Costco'] * 5 +\
+             ['Griff\'s'] * 5 +\
+             ['Tia Betty Blues'] * 5 +\
+             ['Tia Betty Waffles'] * 2 +\
+             ['Saggio\'s'] * 10 +\
+             ['Bahn Mi Coda'] * 10 +\
+             ['Cafe Trang'] * 10 +\
+             ['Freddy\'s'] * 2 +\
+             ['Thai Tip'] * 5 +\
+             ['Vegan Thai'] * 2 +\
+             ['Golden Pride'] +\
+             ['Blake\'s'] +\
+             ['Bob\'s Burger'] * 3 +\
+             ['Sonic'] +\
+             ['The Tractor'] +\
+             ['Lizard Tail'] +\
+             ['La Salita'] * 15 +\
+             ['Chic fil a'] * 2 +\
+             ['Lynns House (call ahead)'] * 40 +\
+             ['Powdrell\'s'] * 10 +\
+             ['The Cube'] +\
+             ['Frontier'] * 3 +\
+             ['Two Fools'] * 5 +\
+             ['Subway'] +\
+             ['McDonald\'s (Kirtland)'] +\
+             ['Burger King'] +\
+             ['Wendy\'s'] +\
+             ['Carl\'s Jr'] +\
+             ['Caesar\'s'] * 3 +\
+             ['Hurricanes'] * 5 +\
+             ['Taco Cabana'] +\
+             ['Chili\'s'] +\
+             ['Appleby\'s'] +\
+             ['TGI Friday\'s'] +\
+             ['B2B'] +\
+             ['Cheese and Coffee'] * 0 +\
+             ['Flying Star Cafe'] +\
+             ['Weck\'s'] +\
+             ['The Owl Cafe'] * 3 +\
+             ['Rudys'] +\
+             ['Cheesecake Factory'] +\
+             ['Chipotle'] +\
+             ['Taco Bell'] +\
+             ['Del Taco'] +\
+             ['Emissions testing place'] +\
+             ['Basil Leaf'] +\
+             ['Chen\'s'] +\
+             ['Fantang'] +\
+             ['Szechwan'] * 5 +\
+             ['Slice Parlor'] +\
+             ['Waffle House'] +\
+             ['Relish'] +\
+             ['Gino\'s New York Style'] +\
+             ['Stufy\'s'] * 3 +\
+             ['Cafe Istanbul (call to make sure their kitchen is open)'] +\
+             ['Taj Palace'] +\
+             ['Olive Garden'] +\
+             ['High Finance'] +\
+             ['Mall food court ()'] +\
+             ['Uptown Sports Pub'] +\
+             ['Panda Express'] +\
+             ['Boston Market'] +\
+             ['Los Cuates'] +\
+             ['El patio'] +\
+             ['Gardunos'] +\
+             ['O Ramen'] +\
+             ['Anapurna'] +\
+             ['Sweet Tomatoes'] +\
+             ['Super Salad'] +\
+             ['Jason\'s Deli'] +\
+             ['McCalister\'s'] * 2 +\
+             ['Golden Corral'] +\
+             ['UNM (the on in the dormitories)'] +\
+             ['Scallo'] +\
+             ['Tucano\'s'] +\
+             ['Copper Lounge'] +\
+             ['Kelly\'s'] +\
+             ['Pizza Hut'] +\
+             ['Domino\'s'] +\
+             ['Godfather Pizza'] +\
+             ['CiCi\'s Pizza'] +\
+             ['Mongolian BBQ'] +\
+             ['Lynn\'s Buffet'] +\
+             ['IHOP'] +\
+             ['Denny\'s'] +\
+             ['Vick\'s Vittles'] * 3 +\
+             ['Twister\'s'] +\
+             ['Hooter\'s,'] +\
+             ['Texas Roadhouse'] +\
+             ['Texas Land and Cattle'] +\
+             ['Santiago\'s'] +\
+             ['Sandia Chille Grille'] +\
+             ['Racetrack Casino'] +\
+             ['Sandia Casino'] +\
+             ['Whataburger'] +\
+             ['Weinerschnitzel'] +\
+             ['Jack in the Box'] +\
+             ['East Mountain Grill'] +\
+             ['Scalo\'s'] +\
+             ['Mannie\'s'] +\
+             ['Mario\'s']
+
 print(random.choice(lunchList))
 print('')
 
